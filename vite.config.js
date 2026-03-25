3c391c5d6a53ae2b2a9a20aa94c0cbd89ee2f170
Armin Pérez Sánchez <arminsanchezlider@gmail.com>
2026-03-25 15:31:57 -0600

    cambio para deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/agronomia-page/' — descomentar cuando crees el repo en GitHub
-  // El nombre debe coincidir con el nombre de tu repositorio en GitHub
+  base: '/webagronomia/',  // Debe coincidir con el nombre de tu repo en GitHub
   server: {
     port: 3000,
   },
